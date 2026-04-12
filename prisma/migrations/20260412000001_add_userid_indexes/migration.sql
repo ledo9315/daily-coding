74d48e2255bac362ee922001b3fd15cea0e9efed
Leonid Domahalskyy <141648130+ledo9315@users.noreply.github.com>
2026-04-12 16:22:28 +0200

    perf: add userId indexes to auth token models

---
+++ prisma/migrations/20260412000001_add_userid_indexes/migration.sql
@@ -0,0 +1,3 @@
+CREATE INDEX "Account_userId_idx" ON "Account"("userId");
+CREATE INDEX "EmailVerificationToken_userId_idx" ON "EmailVerificationToken"("userId");
+CREATE INDEX "PasswordResetToken_userId_idx" ON "PasswordResetToken"("userId");
